88a4f99211e8199e082158ab3057f4ff1b024be4
Ashika Verma <ashika.verma@kensho.com>
2021-07-01 15:19:23 -0500

    render task

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const Web3 = require("web3");
 
 App = {
+  loading: false,
+  contracts: {},
   load: async () => {
     console.log("app loading...");
     await App.connectToBlockchain();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
   },
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   connectToBlockchain: async () => {
@@ -41,6 +46,67 @@ App = {
       console.log(
         "Non-Ethereum browser detected. You should consider trying MetaMask!"
       );
+    }
+  },
+  loadAccount: async () => {
+    App.account = web3.eth.accounts[0];
+  },
+  loadContract: async () => {
+    const todoList = await $.getJSON("TodoList.json");
+    App.contracts.TodoList = TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(App.web3Provider);
+
+    App.todoList = await App.contracts.TodoList.deployed();
+  },
+
+  render: async () => {
+    if (App.loading) return;
+    App.setLoading(true);
+    $("#account").html(App.account);
+    await App.renderTasks();
+
+    App.setLoading(false);
+  },
+  renderTasks: async () => {
+    // Load task count from blockchain
+    const taskCount = await App.todoList.taskCount();
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await App.todoList.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+      console.log(taskId, taskContent, taskCompleted);
+
+      // render each task with task template
+      const $taskTemplate = $(".taskTemplate");
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find(".content").html(taskContent);
+      $newTaskTemplate
+        .find("input")
+        .prop("name", taskId)
+        .prop("checked", taskCompleted);
+      //   .on("click", App.toggleCompleted);
+
+      // show task
+      if (taskCompleted) {
+        $("#completedTaskList").append($newTaskTemplate);
+      } else {
+        $("#taskList").append($newTaskTemplate);
+      }
+      $newTaskTemplate.show();
+    }
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean;
+    const loader = $("#loader");
+    const content = $("#content");
+    if (boolean) {
+      loader.show();
+      content.hide();
+    } else {
+      loader.hide();
+      content.show();
     }
   },
 };
