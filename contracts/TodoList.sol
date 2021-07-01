46e13f09f7c215d6c7d1180152b60ba77ebf0c47
Ashika Verma <ashika.verma@kensho.com>
2021-07-01 14:51:01 -0500

    inject w3 and connect Metamask account

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,5 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint public taskCount = 0; // state variables are written to the block chain
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; // sort of like a dictionary from uint to Task
+
+    constructor() public {
+        createTask("learn solidity");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
 
